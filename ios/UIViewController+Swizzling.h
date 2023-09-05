100cfd7c31766cbd6f66d95e7418c2a17d1da66a
Erick Barbosa <erick.barbosa@teamairship.com>
2023-09-05 06:34:03 -0400

    Added swizziling to the correct directory

---
+++ ios/UIViewController+Swizzling.h
@@ -0,0 +1,16 @@
+//
+//  NSObject+Swizzling.h
+//  RNPdftron
+//
+//  Created by Erick Barbosa on 9/4/23.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UIViewController (Swizzling)
+
+@end
+
+NS_ASSUME_NONNULL_END
