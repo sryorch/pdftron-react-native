bd489624e257b651189104f2a4cea1a646b78ab7
Erick Barbosa <erick.barbosa@teamairship.com>
2023-09-05 15:17:05 -0400

    Saving signature swizzling

---
+++ ios/PTSignaturesManager+Swizzling.h
@@ -0,0 +1,14 @@
+//
+//  NSObject+PTSignatureView.h
+//  RNPdftron
+//
+//  Created by Erick Barbosa on 9/5/23.
+//
+
+#import <UIKit/UIKit.h>
+
+#import <Tools/PTSignaturesManager.h>
+#import <Foundation/Foundation.h>
+
+@interface PTSignaturesManager (Swizzling)
+@end
---
+++ ios/PTSignaturesManager+Swizzling.m
@@ -0,0 +1,34 @@
+#import "PTSignaturesManager+Swizzling.h"
+#import <objc/runtime.h>
+
+@implementation PTSignaturesManager (Swizzling)
+
++ (void)load {
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        Class class = [self class];
+        
+        SEL originalSelector = @selector(createSignature:withStrokeColor:withStrokeThickness:withinRect:saveSignature:);
+        SEL swizzledSelector = @selector(swizzled_createSignature:withStrokeColor:withStrokeThickness:withinRect:saveSignature:);
+        
+        Method originalMethod = class_getInstanceMethod(class, originalSelector);
+        Method swizzledMethod = class_getInstanceMethod(class, swizzledSelector);
+        
+        if (originalMethod && swizzledMethod) {
+            method_exchangeImplementations(originalMethod, swizzledMethod);
+        } else {
+            NSLog(@"Method swizzling failed. Methods not found.");
+        }
+    });
+}
+
+// The swizzled method
+- (PTPDFDoc *)swizzled_createSignature:(NSMutableArray *)points withStrokeColor:(UIColor *)strokeColor withStrokeThickness:(CGFloat)thickness withinRect:(CGRect)rect saveSignature:(BOOL)saveSignature {
+    
+    // Your custom logic here
+    
+    // Call the original method (which is now `swizzled_createSignature:` because we've exchanged their implementations)
+    return [self swizzled_createSignature:points withStrokeColor:strokeColor withStrokeThickness:15.0 withinRect:rect saveSignature:saveSignature];
+}
+
+@end
